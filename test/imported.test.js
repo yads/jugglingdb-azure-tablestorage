9fa5a5b659b08153823c13d60cd96a726b0a3aea
Vadim Kazakov <vadimk@quadrus.com>
2014-11-14 15:52:15 -0700

    unit test fixes
    test timeouts increased
    set npm test command

--- test/imported.test.js
+++ test/imported.test.js
@@ -1,6 +1,7 @@
 require('./init.js');
 
 describe('azure-tablestorage imported features', function() {
+    this.timeout(5000);
 
     require('jugglingdb/test/common.batch.js');
     require('jugglingdb/test/include.test.js');
